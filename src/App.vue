667ae7b3ca2ccb7ad64ce541599e98f5e500080b
Sandhya <sandhumsm15@gmail.com>
2021-05-19 09:21:42 +0530

    todo app created

---
+++ src/App.vue
@@ -0,0 +1,183 @@
+<template>
+	<h1>ToDo App</h1>
+
+ <!--1.Creating Todo form  -->
+ <!--2.Call a function when the form is sumitted -->
+	
+	<form @submit.prevent="addTodo()">
+		<label>New ToDo </label>
+		<input
+			v-model="newTodo"
+			name="newTodo"
+			autocomplete="off">
+		
+		<button>Add ToDo</button>
+	</form>
+ 
+ <!-- Make a Todo list -->
+	
+	<h2>ToDo List</h2>
+	
+	<ul>
+		<li
+			v-for="(todo, index) in todos"
+			:key="index">
+		
+			<span
+				:class="{ done: todo.done }"
+				@click="doneTodo(todo)"
+			>{{ todo.content }}</span>
+			<button @click="removeTodo(index)">Remove</button>
+		</li>
+	</ul>
+	
+	<h4 v-if="todos.length === 0">Empty list.</h4>
+</template>
+
+<!-- script -->
+
+<script>
+	import { ref } from 'vue';
+	export default {
+		name: 'App',
+		setup () {
+			const newTodo = ref('');
+			const todos = ref([]);
+
+// create function for adding todo list into an array
+			
+			function addTodo () {
+				if (newTodo.value) {
+					todos.value.push({
+						done: false,
+						content: newTodo.value
+					});
+					newTodo.value = '';
+				}
+			}
+
+// function for checking todo is done or not
+			
+			function doneTodo (todo) {
+				todo.done = !todo.done
+			}
+
+// function for removing todo
+			function removeTodo (index) {
+				todos.value.splice(index, 1);
+			}
+
+			return {
+				todos,
+				newTodo,
+				addTodo,
+				doneTodo,
+				removeTodo,
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+$border: 2px solid
+	rgba(
+		$color: white,
+		$alpha: 0.35,
+	);
+$size1: 6px;
+$size2: 12px;
+$size3: 18px;
+$size4: 24px;
+$size5: 48px;
+$backgroundColor: #c4c1ad;
+$textColor: rgb(7, 2, 2);
+$primaryColor: #5f51df;
+$secondTextColor: #1f2023;
+body {
+	margin: 0;
+	padding: 0;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	background-color: $backgroundColor;
+	color: $textColor;
+	#app {
+		max-width: 600px;
+		margin-left: auto;
+		margin-right: auto;
+		padding: 20px;
+		h1 {
+			font-weight: bold;
+			font-size: 28px;
+			text-align: center;
+		}
+		form {
+			display: flex;
+			flex-direction: column;
+			width: 100%;
+			label {
+				font-size: 14px;
+				font-weight: bold;
+			}
+			input,
+			button {
+				height: $size5;
+				box-shadow: none;
+				outline: none;
+				padding-left: $size2;
+				padding-right: $size2;
+				border-radius: $size1;
+				font-size: 18px;
+				margin-top: $size1;
+				margin-bottom: $size2;
+			}
+			input {
+				background-color: transparent;
+				border: $border;
+				color: inherit;
+			}
+		}
+		button {
+			cursor: pointer;
+			background-color: $primaryColor;
+			border: 1px solid $primaryColor;
+			color: $secondTextColor;
+			font-weight: bold;
+			outline: none;
+			border-radius: $size1;
+		}
+		h2 {
+			font-size: 22px;
+			border-bottom: $border;
+			padding-bottom: $size1;
+		}
+		ul {
+			padding: 10px;
+			li {
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				border: $border;
+				padding: $size2 $size4;
+				border-radius: $size1;
+				margin-bottom: $size2;
+				span {
+					cursor: pointer;
+				}
+				.done {
+					text-decoration: line-through;
+				}
+				button {
+					font-size: $size2;
+					padding: $size1;
+				}
+			}
+		}
+		h4 {
+			text-align: center;
+			opacity: 0.5;
+			margin: 0;
+		}
+	}
+}
+</style>
